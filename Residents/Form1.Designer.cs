299042fb0183a56efa4596ab5ed8ef4dc2dc6f0e
Honcharuk <manager@HONCHARUK>
2019-05-13 11:20:19 +0300

    Added search

--- Residents/Form1.Designer.cs
+++ Residents/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBoxSearch = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripComboBoxSearcType = new System.Windows.Forms.ToolStripComboBox();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridViewResidents = new System.Windows.Forms.DataGridView();
             this.menuStrip1.SuspendLayout();
@@ -41,24 +44,52 @@
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.infoToolStripMenuItem,
+            this.toolStripTextBoxSearch,
+            this.toolStripComboBoxSearcType,
+            this.findToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(782, 28);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(588, 27);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // infoToolStripMenuItem
             // 
             this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
-            this.infoToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 23);
             this.infoToolStripMenuItem.Text = "Info";
             this.infoToolStripMenuItem.Click += new System.EventHandler(this.infoToolStripMenuItem_Click);
+            // 
+            // toolStripTextBoxSearch
+            // 
+            this.toolStripTextBoxSearch.Name = "toolStripTextBoxSearch";
+            this.toolStripTextBoxSearch.Size = new System.Drawing.Size(120, 23);
+            this.toolStripTextBoxSearch.ToolTipText = "Параметр пошуку";
+            // 
+            // toolStripComboBoxSearcType
+            // 
+            this.toolStripComboBoxSearcType.IntegralHeight = false;
+            this.toolStripComboBoxSearcType.Items.AddRange(new object[] {
+            "Ім*я",
+            "Прізвище"});
+            this.toolStripComboBoxSearcType.MaxDropDownItems = 2;
+            this.toolStripComboBoxSearcType.Name = "toolStripComboBoxSearcType";
+            this.toolStripComboBoxSearcType.Size = new System.Drawing.Size(90, 23);
+            this.toolStripComboBoxSearcType.ToolTipText = "Тип пошуку";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(42, 23);
+            this.findToolStripMenuItem.Text = "Find";
+            this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(45, 24);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 23);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -67,21 +98,23 @@
             this.dataGridViewResidents.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewResidents.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewResidents.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridViewResidents.Location = new System.Drawing.Point(0, 28);
+            this.dataGridViewResidents.Location = new System.Drawing.Point(0, 27);
+            this.dataGridViewResidents.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridViewResidents.Name = "dataGridViewResidents";
             this.dataGridViewResidents.RowTemplate.Height = 24;
-            this.dataGridViewResidents.Size = new System.Drawing.Size(782, 425);
+            this.dataGridViewResidents.Size = new System.Drawing.Size(588, 341);
             this.dataGridViewResidents.TabIndex = 1;
             // 
             // Form_Residents
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.ClientSize = new System.Drawing.Size(588, 368);
             this.Controls.Add(this.dataGridViewResidents);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.MinimumSize = new System.Drawing.Size(800, 400);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(604, 332);
             this.Name = "Form_Residents";
             this.ShowIcon = false;
             this.Text = "     Residents";
@@ -99,6 +132,9 @@
         private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.DataGridView dataGridViewResidents;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxSearch;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxSearcType;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
     }
 }
 
--- Residents/Form1.cs
+++ Residents/Form1.cs
@@ -72,5 +72,63 @@ namespace WindowsFormsApplication1
                 MessageBox.Show(ex.Message);
             }
         }
+
+        void searchResident(string SearchParam, string SearchType) {
+            string myConnection = "server=web113.default-host.net;Port=3306;uid=daleth_max;pwd=huE96m5VHbC3;database=daleth_warehouse";
+            MySqlConnection myConnect = new MySqlConnection(myConnection);
+            
+            string selectQuery = "";
+
+            if (SearchType == "Ім*я")
+            {
+                selectQuery = "SELECT * FROM daleth_warehouse.resident_table WHERE resident_name = '"+ SearchParam + "';";
+            }
+            if (SearchType == "Прізвище")
+            {
+                selectQuery = "SELECT * FROM daleth_warehouse.resident_table WHERE resident_sec_name = '" + SearchParam + "';";
+            }
+
+
+            try
+            {
+                myConnect.Open();
+
+                MySqlCommand command = new MySqlCommand(selectQuery, myConnect);
+
+                MySqlDataAdapter dataAdapter = new MySqlDataAdapter();
+
+                dataAdapter.SelectCommand = command;
+                DataTable dTable = new DataTable();
+
+                dataAdapter.Fill(dTable);
+
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dTable;
+                dataGridViewResidents.DataSource = bSource;
+                dataAdapter.Update(dTable);
+
+                dataGridViewResidents.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.DisplayedCells;
+
+                dataGridViewResidents.Columns[0].HeaderText = "ID";
+                dataGridViewResidents.Columns[1].HeaderText = "Name";
+                dataGridViewResidents.Columns[2].HeaderText = "Sec. name";
+                dataGridViewResidents.Columns[3].HeaderText = "Address";
+
+                myConnect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if( this.toolStripTextBoxSearch.Text != "" && this.toolStripComboBoxSearcType.Text != "")
+            {
+                searchResident(this.toolStripTextBoxSearch.Text, this.toolStripComboBoxSearcType.Text);
+            }
+        }
     }
 }
